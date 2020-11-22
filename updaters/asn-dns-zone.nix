fe8b4c90854cf903316e594ba9b382ee85fcb007
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-11-22 03:23:59 -0300

    updaters/asn-dns-zone: Drop TTL from records, use the default from $TTL

--- updaters/asn-dns-zone.nix
+++ updaters/asn-dns-zone.nix
@@ -5,27 +5,32 @@
 {
   # Template updater for the dn42 ASN records.
   #
-  # Be sure to set the destination file and reload scripts, and to set
-  # an `$ORIGIN` and SOA and NS records using `mkBefore` in the process
-  # script.
+  # What here needs to be set for it to be complete:
+  # - `destination`
+  # - `reload` or `services`
+  # - `interval`
+  #
+  # What needs to be prepended to `process` via `mkBefore`:
+  # - `$ORIGIN`
+  # - `$TTL`
+  # - `SOA`
+  # - `NS`
   dn42.updaters.asn = {
     source = "https://explorer.burble.com/api/registry/aut-num/*?raw";
     processPackages = [ pkgs.glibc.bin pkgs.jq ];
     process =
       let
-        ttl = "86400";
         filter = pkgs.writeText "jq-dn42-asn-filter" ''
           .[]
           | map({key: .[0], value: .[1]})
           | from_entries
           | "\(."aut-num" | ltrimstr("AS")) | | \(.source | ascii_downcase) | | \(."as-name")" as $rdata
-          | "\(."aut-num") \($ttl) IN TXT \"\($rdata)\""
+          | "\(."aut-num") IN TXT \"\($rdata)\""
         '';
       in
       ''
-        jq --from-file "${filter}" --arg ttl "${ttl}" --raw-output \
+        jq --from-file "${filter}" --raw-output \
         | iconv --from-code=UTF-8 --to-code=ASCII//TRANSLIT
       '';
-    interval = "1day";
   };
 }
