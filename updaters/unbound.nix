f78b9aec007d5b6e8a95f854988a07209e17daec
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2021-04-22 08:54:21 -0300

    Revert "updaters/unbound: Add trust anchors to reverse zones"
    This reverts commit 73018a63167ffbdaada6dc659f593042631a8543.  I often
    forget, but DS records include the domain name in the hashed rdata, so
    that a DS record for dn42. is not valid for d.f.ip6.arpa. even if it is
    the same DNSKEY.

--- updaters/unbound.nix
+++ updaters/unbound.nix
@@ -1,26 +1,7 @@
 # SPDX-FileCopyrightText: 2020 Aluísio Augusto Silva Gonçalves <https://aasg.name>
 # SPDX-License-Identifier: MIT
 
-{ lib, pkgs, ... }:
-let
-  argJSON = v: lib.escapeShellArg (builtins.toJSON v);
-
-  # List of zones queried from the dn42 nameservers.
-  dn42_zones = [
-    "dn42."
-    "20.172.in-addr.arpa."
-    "21.172.in-addr.arpa."
-    "22.172.in-addr.arpa."
-    "23.172.in-addr.arpa."
-    "d.f.ip6.arpa."
-  ];
-  # List of zones queried from the NeoNetwork nameservers.
-  neonetwork_zones = [
-    "neo."
-    "127.10.in-addr.arpa."
-    "7.2.1.0.0.1.d.f.ip6.arpa."
-  ];
-in
+{ pkgs, ... }:
 {
   # Update the dn42 root servers and DNSSEC keys regularly.
   dn42.updaters.unbound-roots = {
@@ -41,7 +22,7 @@ in
     defaultContents = "";
     processPackages = [ pkgs.jq ];
     process = ''
-      jq --raw-output --argjson zones ${argJSON dn42_zones} '."domain/dn42"."ds-rdata" | .[] as $rdata | $zones[] as $zone | "\($zone) IN DS \($rdata)"'
+      jq --raw-output '."domain/dn42"."ds-rdata" | .[] | "dn42. IN DS \(.)"'
     '';
     services = [ "unbound.service" ];
     interval = "1day";
@@ -53,7 +34,7 @@ in
     defaultContents = "";
     processPackages = [ pkgs.jq ];
     process = ''
-      jq --raw-output --argjson zones ${argJSON neonetwork_zones} '."domain/neo"."ds-rdata" | .[] as $rdata | $zones[] as $zone | "\($zone) IN DS \($rdata)"'
+      jq --raw-output '."domain/neo"."ds-rdata" | .[] | "dn42. IN DS \(.)"'
     '';
     services = [ "unbound.service" ];
     interval = "1day";
